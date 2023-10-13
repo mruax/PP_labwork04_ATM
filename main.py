963837867226e9658037b790c1da5920221e12eb
mruax <matvey.chernyakov@yandex.ru>
2023-10-13 20:02:35 +0500

    Added easy version :)

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import pymorphy2
+
+
 if __name__ == "__main__":
-    pass
+    morph = pymorphy2.MorphAnalyzer()
+    ans = morph.parse('рубль')[0]
+
+    n = input("Введите сумму от 1 до 100000: ")
+    while type(n) is str:
+        try:
+            n = int(n)
+            if not(1 <= n <= 100000):
+                raise Exception
+        except Exception:
+            n = input("Неверно! Введите сумму от 1 до 100000: ")
+    print(n, ans.make_agree_with_number(n).word)
